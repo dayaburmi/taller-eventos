290b23a599478c454907521a605b4e33a829b317
dayaburmi <dayanaburmida@gmail.com>
2023-08-08 23:10:16 -0300

    ultimo commit

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Obtengo una referencia al elemento div por su ID
+let elementoDiv = document.getElementById("contenedor");
+
+// Agrego un manejador de eventos para el evento "click"
+elementoDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
