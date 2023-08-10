7d198ce8ffce0a25fb636c0a171347d2f9b2a2ce
dayaburmi <dayanaburmida@gmail.com>
2023-08-09 22:54:40 -0300

    actualizacion

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
-function cartelSaludo() {      // Función para mostrar el mensaje de saludo
+let buton = document.getElementById("botonSaludo")
+
+buton.addEventListener("click", function(event) {
     alert("¡Hola!");
-}
+    event.stopPropagation(); // Detiene la propagación del evento al div
+})
