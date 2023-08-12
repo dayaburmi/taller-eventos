5e894113abe9bb68adc540d9c090a0b0221f9298
dayaburmi <dayanaburmida@gmail.com>
2023-08-11 23:11:08 -0300

    Solucion al cartel extra en el boton
    Se añade la funcion stopPropagation() para evitar que se propague el evento del click al div.

--- scripts.js
+++ scripts.js
@@ -2,4 +2,5 @@ let buton = document.getElementById("botonSaludo")
 
 buton.onclick = function(event) {
     alert("¡Hola!");
+    event.stopPropagation(); // Detiene la propagacion del evento hacia el div
 }
